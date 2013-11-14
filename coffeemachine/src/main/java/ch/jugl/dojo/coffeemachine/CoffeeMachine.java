db24c722538c53aa4e2664f62d01bd2ddddfca8b
Farhdine Boutzakhti <farhdine@octofbi.local>
2013-11-14 20:55:31 +0100

    1st part

--- coffeemachine/src/main/java/ch/jugl/dojo/coffeemachine/CoffeeMachine.java
+++ coffeemachine/src/main/java/ch/jugl/dojo/coffeemachine/CoffeeMachine.java
@@ -1,5 +1,35 @@
 package ch.jugl.dojo.coffeemachine;
 
 public class CoffeeMachine {
+	
+	public int sous = 0;
+	
+	public void setMoney(int sous) {
+		this.sous = sous;
+	}
+	
+	public static enum Beverage {
+		TEA("T",40), CHOCOLATE("H",50), COFFEE("C",60);
+		String code;
+		int price;
+		private Beverage(String code, int price) {
+			this.code = code;
+			this.price = price;
+		}
+	}; 
+	
+	public String order(Beverage beverage, int sugarQuantity) {
+		if(sous<beverage.price) {
+			return sendMessage("0," + (beverage.price - sous) + " missing");
+		}
+		return beverage.code + ":"+ (sugarQuantity > 0 ? sugarQuantity +":0" : ":");
+	}
+
+	public String sendMessage(String string) {
+		return "M:"+string;
+	}
+	
+	
 
 }
+
--- coffeemachine/src/test/java/ch/jugl/dojo/coffeemachine/CoffeeMachineTest.java
+++ coffeemachine/src/test/java/ch/jugl/dojo/coffeemachine/CoffeeMachineTest.java
@@ -1,5 +1,118 @@
 package ch.jugl.dojo.coffeemachine;
 
-public class CoffeeMachineTest {
+import junit.framework.TestCase;
+import ch.jugl.dojo.coffeemachine.CoffeeMachine.Beverage;
 
+public class CoffeeMachineTest extends TestCase {
+
+	private CoffeeMachine cf;
+
+	public void setUp() {
+		cf = new CoffeeMachine();
+		cf.setMoney(100);
+	}
+	
+	public void testOrderTeaWhithoutSugar() {
+		assertEquals("T::", cf.order(Beverage.TEA, 0));
+	}
+	
+	public void testOrderTeaWhithOneSugar() {
+		assertEquals("T:1:0", cf.order(Beverage.TEA, 1));
+	}
+	
+	public void testOrderTeaWhithTwoSugar() {
+		assertEquals("T:2:0", cf.order(Beverage.TEA, 2));
+	}
+	
+	public void testOrderChocolateWhithoutSugar() {
+		assertEquals("H::", cf.order(Beverage.CHOCOLATE, 0));
+	}
+
+	public void testOrderChocolateWhithOneSugar() {
+		assertEquals("H:1:0", cf.order(Beverage.CHOCOLATE, 1));
+	}
+	
+	public void testOrderCoffeeWhithoutSugar() {
+		assertEquals("C::", cf.order(Beverage.COFFEE, 0));
+	}
+	
+	public void testOrderCoffeeWithOneSugar() {
+		assertEquals("C:1:0", cf.order(Beverage.COFFEE, 1));
+	}
+	
+	public void testShowMessage(){
+		assertEquals("M:message", cf.sendMessage("message"));
+	}
+
+	public void testNoMoneyNoTea() { 
+		cf.setMoney(0);
+		assertEquals("M:0,40 missing", cf.order(Beverage.TEA, 1));
+	}
+	
+	public void testNotEnoughMoneyForATea() { 
+		cf.setMoney(30);
+		assertEquals("M:0,10 missing", cf.order(Beverage.TEA, 1));
+	}
+	
+	
+	public void testNotEnoughMoneyForACoffee() { 
+		cf.setMoney(30);
+		assertEquals("M:0,30 missing", cf.order(Beverage.COFFEE, 1));
+	}
+	
+	public void testNotEnoughMoneyForAChocolate() { 
+		cf.setMoney(00);
+		assertEquals("M:0,50 missing", cf.order(Beverage.CHOCOLATE, 1));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
